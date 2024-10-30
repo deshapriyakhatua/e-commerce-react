2e94f7a5b9a157c686fdf7d0115557028ab50a84
deshapriya <deshapriyakhatua@gmail.com>
2024-10-30 19:41:38 +0530

    checkout page added

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Checkout.module.css'
 
 function Checkout() {
 
+    const [isCashOnDelivery, setIsCashOnDelivery] = useState(true);
     const products = [
         {
-            imgSrc: '',
+            imgSrc: 'https://apisap.fabindia.com/medias/20174793-01.jpg?context=bWFzdGVyfGltYWdlc3wxNjMzMDJ8aW1hZ2UvanBlZ3xhREF5TDJnMU1pODFNakl4T1Rnd01UZ3lPVFF3Tmk4eU1ERTNORGM1TTE4d01TNXFjR2N8YmZkMWQ4YWRlMDM3YjMxMTAxYjQ3ZGJlZmUxYjQyNjM4NTgxZmI2MTNhM2ZmOTIzZTdjN2YzYmQ1MDc1N2EyMA',
             title: 'Latest Colored shirt',
-            price: 500,
-            qnty: 2
+            price: 250,
+            qnty: 20
         },
         {
-            imgSrc: '',
+            imgSrc: 'https://apisap.fabindia.com/medias/20174793-02.jpg?context=bWFzdGVyfGltYWdlc3wxMjA4ODJ8aW1hZ2UvanBlZ3xhRFJsTDJoaE55ODFNakl4T1Rnd01qSXlNall5TWk4eU1ERTNORGM1TTE4d01pNXFjR2N8OTBkMmZmNDBiMDJjYjEyMGY5OTFlMGFiNmM0MzJjZmRlMWRiYzMyZDgyZGMyMDlhYjFlOTcyODBkYjkwNzRkMw',
             title: 'Latest Colored shirt',
-            price: 500,
-            qnty: 2
+            price: 542,
+            qnty: 1
         },
         {
-            imgSrc: '',
+            imgSrc: 'https://apisap.fabindia.com/medias/20174793-03.jpg?context=bWFzdGVyfGltYWdlc3wxNTk1Nzh8aW1hZ2UvanBlZ3xhRE16TDJoaU5TODFNakl4T1Rnd01qWTBPRFl3Tmk4eU1ERTNORGM1TTE4d015NXFjR2N8ZGI1YzdlOTkyMzgxYjVmZjAyM2JhNjBkN2Y1ZGZlZTRkOTYzM2JjMDc2MWVkNGVmZmI2ZjQ0NGU3N2E5N2M5OQ',
             title: 'Latest Colored shirt',
-            price: 500,
-            qnty: 2
+            price: 354,
+            qnty: 5
         },
-    ]
+    ];
+
     return (
         <section className={styles.mainContainer}>
 
@@ -69,7 +71,7 @@ function Checkout() {
                             </div>
                         </div>
                         <div className={styles.checkboxContainer}>
-                            <input id='saveAddress' type='checkbox' />
+                            <input id='saveAddress' type='checkbox' defaultChecked/>
                             <label htmlFor="saveAddress">Save Address</label>
                         </div>
                     </div>
@@ -89,10 +91,19 @@ function Checkout() {
                     <div className={styles.payment}>
                         <h1>Payment</h1>
                         <div className={styles.checkboxContainer}>
-                            <input id='cashOnDelivery' type='checkbox' />
+                            <input
+                                id="cashOnDelivery"
+                                type="checkbox"
+                                checked={isCashOnDelivery}
+                                onChange={(e) => {
+                                    setIsCashOnDelivery(e.target.checked);
+                                }}
+                            />
                             <label htmlFor="cashOnDelivery">Cash On Delivery</label>
                         </div>
-                        <button>Pay Now</button>
+                        {!isCashOnDelivery && (
+                            <button className={styles.payNow}>Pay Now</button>
+                        )}
                     </div>
 
                 </div>
@@ -107,7 +118,7 @@ function Checkout() {
                                     <span className={styles.productQnty}>{elem.qnty}</span>
                                 </div>
                                 <p className={styles.productName}>{elem.title}</p>
-                                <p className={styles.productPrice}>{elem.price}</p>
+                                <p className={styles.productPrice}>{elem.price} Rs</p>
                             </div>
                         ))}
                     </div>
@@ -115,15 +126,15 @@ function Checkout() {
                     <div className={styles.priceContainer}>
                         <div className={styles.subtotal}>
                             <p>Subtotal</p>
-                            <p>255</p>
+                            <p>255 Rs</p>
                         </div>
                         <div className={styles.shipping}>
                             <p>Shipping</p>
-                            <p>55</p>
+                            <p>55 Rs</p>
                         </div>
                         <div className={styles.total}>
                             <p>Total</p>
-                            <p>310</p>
+                            <p>310 Rs</p>
                         </div>
                     </div>
 
