be8b0f94ca78104b589afec2f26a00ab34b63098
deshapriya <deshapriyakhatua@gmail.com>
2024-11-08 12:47:56 +0530

    working on FAQ page

--- src/components/fAQ/fAQSection/FAQSection.jsx
+++ src/components/fAQ/fAQSection/FAQSection.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './FAQSection.module.css'
+import { IoIosArrowDown } from 'react-icons/io'
 
-function FAQSection() {
+function FAQSection({ data }) {
+
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+
   return (
-    <div>FAQSection</div>
+    <div className={styles.mainSection}>
+      <h1>{data.heading}</h1>
+      <p>{data.shortText}</p>
+      <div>
+        {data.faqs && data.faqs.map((elem, indx) => (
+          <div>
+            <div onClick={() => { setSelectedQuestion(elem.q); }}>
+            <h2>{elem.q}</h2>
+            <IoIosArrowDown className={selectedQuestion === elem.q && styles.rotateIcon}/>
+            </div>
+            {selectedQuestion && selectedQuestion === elem.q && (
+              <p>{elem.a}</p>
+            )}
+          </div>
+
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/pages/fAQ/FAQ.jsx
+++ src/pages/fAQ/FAQ.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
 import styles from './FAQ.module.css'
+import FAQSection from '../../components/fAQ/fAQSection/FAQSection'
 
 function FAQ() {
+  const data = [
+    {
+      heading: 'Orders',
+      shortText: 'Below are some of are common questions about orders',
+      faqs: [
+        {
+          q: 'How do I make a purchase ?',
+          a: `This is an example of a response that you might give. It's good to be as thorough as possible in responses as that has a tendency to improve trust overall.`
+        },
+        {
+          q: 'How do I know if my order is confirmed ?',
+          a: `This is an example of a response that you might give. It's good to be as thorough as possible in responses as that has a tendency to improve trust overall.`
+        },
+        {
+          q: 'Can I change my shipping address after my order is placed ?',
+          a: `This is an example of a response that you might give. It's good to be as thorough as possible in responses as that has a tendency to improve trust overall.`
+        }
+      ]
+    },
+    {
+      heading: 'Shipping & Returns',
+      shortText: 'Below are some of are common questions about Shipping & Returns',
+      faqs: [
+        {
+          q: 'How do I make a purchase ?',
+          a: `This is an example of a response that you might give. It's good to be as thorough as possible in responses as that has a tendency to improve trust overall.`
+        },
+        {
+          q: 'How do I know if my order is confirmed ?',
+          a: `This is an example of a response that you might give. It's good to be as thorough as possible in responses as that has a tendency to improve trust overall.`
+        },
+        {
+          q: 'Can I change my shipping address after my order is placed ?',
+          a: `This is an example of a response that you might give. It's good to be as thorough as possible in responses as that has a tendency to improve trust overall.`
+        }
+      ]
+    },
+  ]
   return (
     <div className={styles.mainPage}>
         <div className={styles.mainContainer}>
-            <section></section>
+          {data && data.map((elem, indx) => (
+            <FAQSection key={indx} data={elem}/>
+          ))}
         </div>
     </div>
   )
